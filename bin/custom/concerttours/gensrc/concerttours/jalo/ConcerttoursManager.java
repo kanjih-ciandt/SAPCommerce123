d3e48d2099e466dbef396164b3ebd2680ab62cd1
Kanji Hara Neto <hkanjih@gmail.com>
2023-08-21 10:43:19 -0300

    Create a Custom Event

--- bin/custom/concerttours/gensrc/concerttours/jalo/ConcerttoursManager.java
+++ bin/custom/concerttours/gensrc/concerttours/jalo/ConcerttoursManager.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Aug 21, 2023, 10:13:01 AM                   ---
+ * --- Generated at Aug 21, 2023, 10:30:14 AM                   ---
  * ----------------------------------------------------------------
  */
 package concerttours.jalo;
